ff7dda021c1fc2adba7322d3e567049ccecb4588
James <james@example.com>
2014-10-16 21:24:41 +0100

    JP: improve the spec language

--- spec/lib/waiter_spec.rb
+++ spec/lib/waiter_spec.rb
@@ -3,79 +3,83 @@ require_relative '../../lib/waiter.rb'
 describe Waiter do
   include Waiter
 
-  it 'should take a block and yield to it' do
-    expect { |b| wait_until(&b) }.to yield_control
-  end
-
-  context 'block evaluates to true' do
-    it 'should return true' do
-      expect(wait_until { true }).to be_true
+  describe '#wait_until' do
+    it 'takes a block and yields to it' do
+      expect { |b| wait_until(&b) }.to yield_control
     end
 
-    it 'should return immediately' do
-      start_time = time_now
-
-      wait_until { true }
-
-      expect(time_now - start_time).to be <= 0.01
-    end
-  end
-
-  context 'timeout has been reached' do
-    it 'should throw an exception' do
-      expect { wait_until(timeout: 2) { false } }.to raise_error(Waiter::WaiterError)
-    end
-  end
-
-  context 'timeout has not been reached' do
-    shared_examples 'a block executer that keeps waiting and trying' do
-      it 'should keep trying' do
-        counter = 0
-
-        proc = Proc.new { counter += 1; result.call }
-
-        begin
-          wait_until(timeout: 3, sleep: 1, &proc)
-        rescue
-        end
-
-        expect(counter).to be >= 2
+    context 'block evaluates to true' do
+      it 'returns true' do
+        expect(wait_until { true }).to be_true
       end
 
-      it 'should pause between executions' do
-        counter = 0
-
-        proc = Proc.new { counter += 1; result.call }
-
-        begin
-          wait_until(timeout: 3, sleep: 1, &proc)
-        rescue
-        end
-
-        expect(counter).to be_within(1).of(3)
-      end
-
-      it 'should keep trying for the time specified' do
+      it 'returns immediately' do
         start_time = time_now
 
-        begin
-          wait_until(timeout: 3, sleep: 1) { result.call }
-        rescue
-        end
+        wait_until { true }
 
-        expect(time_now - start_time).to be >= 3
+        expect(time_now - start_time).to be <= 0.01
       end
     end
 
     context 'block evaluates to false' do
-      it_behaves_like 'a block executer that keeps trying' do
-        let(:result) { false }
+      context 'timeout has been reached' do
+        it 'throws an exception' do
+          expect { wait_until(timeout: 2) { false } }.to raise_error(Waiter::WaiterError)
+        end
       end
-    end
 
-    context 'block raises exception' do
-      it_behaves_like 'a block executer that keeps trying' do
-        let(:result) { raise StandardError }
+      context 'timeout has not been reached' do
+        shared_examples 'a block executer that keeps trying and waiting' do
+          it 'keeps trying' do
+            counter = 0
+
+            proc = Proc.new { counter += 1; result.call }
+
+            begin
+              wait_until(timeout: 3, sleep: 1, &proc)
+            rescue
+            end
+
+            expect(counter).to be >= 2
+          end
+
+          it 'pauses between executions' do
+            counter = 0
+
+            proc = Proc.new { counter += 1; result.call }
+
+            begin
+              wait_until(timeout: 3, sleep: 1, &proc)
+            rescue
+            end
+
+            expect(counter).to be_within(1).of(3)
+          end
+
+          it 'keeps trying for the time specified' do
+            start_time = time_now
+
+            begin
+              wait_until(timeout: 3, sleep: 1) { result.call }
+            rescue
+            end
+
+            expect(time_now - start_time).to be >= 3
+          end
+        end
+
+        context 'block evaluates to false' do
+          it_behaves_like 'a block executer that keeps trying and waiting' do
+            let(:result) { false }
+          end
+        end
+
+        context 'block raises exception' do
+          it_behaves_like 'a block executer that keeps trying and waiting' do
+            let(:result) { raise StandardError }
+          end
+        end
       end
     end
   end
